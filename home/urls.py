e900701f3949691d853f9171ae53dc1122d0b599
aungminthant13 <a.minthant2003@gmail.com>
2025-02-24 01:27:05 +0700

    homepage reset up, ready to design

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
 ]
+
+# from django.urls import path
+# from .views import home
+
+# urlpatterns = [
+#     path('', home, name='home'),
+# ]
--- home/views.py
+++ home/views.py
@@ -4,8 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    content1 = "<br><h2>Welcome! The web app is currently under construction!</h2> <br><br><br>"
-    content2 = "Meanwhile, listen to this Jazz playlist. <br><br>"
-    content3 = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DWV7EzJMK2FUI?utm_source=generator" width="70%" height="500" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
-    content = "<center>" + content1 + content2 + content3 + "</center>"
-    return HttpResponse(content)
+    return render(request, 'home/index.html')
