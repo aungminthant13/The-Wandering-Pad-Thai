8d3b69f072c90a22edb52638ebd6d4771b0d0a20
aungminthant13 <a.minthant2003@gmail.com>
2025-05-30 02:30:20 +0700

    minor changes on homepage -> itinerary update

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('trips/', views.trips, name='trips'),
+    path("auth/", include("user_auth.urls")),
+
 ]
 
 # from django.urls import path
